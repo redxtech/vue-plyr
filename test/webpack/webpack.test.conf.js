d168061928a4051656c56fe97eaaf5da8e5c0dd3
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-01-29 23:28:28 -0700

    add tests

---
+++ test/webpack/webpack.test.conf.js
@@ -0,0 +1,20 @@
+const vueLoaderConfig = require('./vue-loader.conf')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }
+    ]
+  }
+}
