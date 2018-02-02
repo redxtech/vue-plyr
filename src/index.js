17bdb0bd6f35c4436e0fb1058c701a3818948186
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-02-01 23:17:00 -0700

    fix youtube capitolization in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import PlyrVideo from './plyr-video.vue'
-import PlyrYouTube from './plyr-youtube.vue'
+import PlyrYoutube from './plyr-youtube.vue'
 import PlyrVimeo from './plyr-vimeo.vue'
 import PlyrAudio from './plyr-audio.vue'
 
 import PlyrVideoSlot from './plyr-video-slot.vue'
 import PlyrAudioSlot from './plyr-audio-slot.vue'
 import PlyrVimeoSlot from './plyr-vimeo-slot.vue'
-import PlyrYouTubeSlot from './plyr-youtube-slot.vue'
+import PlyrYoutubeSlot from './plyr-youtube-slot.vue'
 
-const Components = [PlyrVideo, PlyrYouTube, PlyrVimeo, PlyrAudio, PlyrVideoSlot, PlyrAudioSlot, PlyrVimeoSlot, PlyrYouTubeSlot]
+const Components = [PlyrVideo, PlyrYoutube, PlyrVimeo, PlyrAudio, PlyrVideoSlot, PlyrAudioSlot, PlyrVimeoSlot, PlyrYoutubeSlot]
 
 export default Vue => {
   Components.forEach((Component) => {
@@ -19,10 +19,10 @@ export default Vue => {
 export {
   PlyrVideo,
   PlyrAudio,
-  PlyrYouTube,
+  PlyrYoutube,
   PlyrVimeo,
   PlyrVideoSlot,
   PlyrAudioSlot,
   PlyrVimeoSlot,
-  PlyrYouTubeSlot
+  PlyrYoutubeSlot
 }
