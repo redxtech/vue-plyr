645c967613f67d915ca72c628ccd0b225d2cd8c6
Radames Ajna <radamajna@gmail.com>
2018-05-31 14:47:51 -0700

    use polyfilled plyr version

--- src/plyr-vimeo.vue
+++ src/plyr-vimeo.vue
@@ -57,7 +57,7 @@
       }
     },
     mounted () {
-      const Plyr = require('plyr')
+      const Plyr = require('plyr-polyfilled')
       this.player = new Plyr(document.getElementById(`js-player-vimeo-${this.idNumber}`), this.options)
       this.emit.forEach(element => {
         this.player.on(element, this.emitPlayerEvent)
