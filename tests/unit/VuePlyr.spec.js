b689f40eaff8887a20dafe0482fe49d0d1eba223
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-10-17 21:45:22 -0600

    remove useless comment

--- tests/unit/VuePlyr.spec.js
+++ tests/unit/VuePlyr.spec.js
@@ -5,6 +5,5 @@ describe('VuePlyr.vue', () => {
   it('Is a vue instance.', () => {
     const wrapper = shallowMount(VuePlyr)
     expect(wrapper.isVueInstance()).toBeTruthy()
-    // expect(wrapper.text()).toMatch(op)
   })
 })
