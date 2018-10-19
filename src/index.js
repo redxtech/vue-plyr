072eba39beb1c50092e9ab3f4f54723f9732cf60
Irfaq Syed <irazasyed@users.noreply.github.com>
2018-10-19 09:52:22 +0530

    Revisions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 import VuePlyr from './VuePlyr.vue'
 
 const Components = {
-    VuePlyr
+  VuePlyr
 }
 
 const VuePlyrPlugin = {
-    install(Vue, options) {
-        Object.keys(Components).forEach(component => {
-            Vue.component(Components[component].name, Components[component])
-        })
-    }
-}
-
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(VuePlyrPlugin)
+  install (Vue, options) {
+    Object.keys(Components).forEach(component => {
+      Vue.component(Components[component].name, Components[component])
+    })
+  }
 }
 
 export {
-    VuePlyrPlugin,
-    VuePlyr,
+  VuePlyrPlugin,
+  VuePlyr
 }
 
 export default VuePlyrPlugin
