8979711fc0b5403064b222889bcaed99acce9625
zcuric <zcuric@gmail.com>
2019-03-14 12:44:41 +0100

    Update bili and babel config to transpile vue

--- bili.config.js
+++ bili.config.js
@@ -1,13 +1,18 @@
+const babel = require('rollup-plugin-babel')
+
 module.exports = {
   input: 'src/index.js',
   outDir: 'dist',
   format: ['cjs', 'es', 'umd'],
   moduleName: 'VuePlyr',
-  js: 'babel',
   postcss: {
     extract: true
   },
   plugins: [
-    'vue'
+    'vue',
+    babel({
+      runtimeHelpers: true,
+      extensions: ['.js', '.vue']
+    })
   ]
 }
