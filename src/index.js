765f573cff3147e0328dec453953483053a92caf
Davide Rossi <dappiu@gmail.com>
2019-03-19 20:58:30 +0100

    Removes unnecessary iteration in the plugin install function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import VuePlyr from './VuePlyr.vue'
 
-const Components = {
-  VuePlyr
-}
-
 const VuePlyrPlugin = {
   install (Vue, options) {
-    Object.keys(Components).forEach(component => {
-      Vue.component(Components[component].name, Components[component])
-    })
+    Vue.component(VuePlyr.name, VuePlyr)
   }
 }
 
