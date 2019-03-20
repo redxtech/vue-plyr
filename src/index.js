ecd99ebd17b6936c74cd81fbb0b41e6086142cc3
Davide Rossi <dappiu@gmail.com>
2019-03-20 12:30:21 +0100

    Fixes mixed use of named and default exports by injecting the install method in the main component

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import VuePlyr from './VuePlyr.vue'
 
-const VuePlyrPlugin = {
-  install (Vue, options = {}) {
-    if (options.plyr) {
-      VuePlyr.props.options.default = () => { return { ...options.plyr } }
-    }
-    if (options.emit) {
-      VuePlyr.props.emit.default = () => { return [ ...options.emit ] }
-    }
-    Vue.component(VuePlyr.name, VuePlyr)
+VuePlyr.install = (Vue, options = {}) => {
+  if (options.plyr) {
+    VuePlyr.props.options.default = () => { return { ...options.plyr } }
   }
+  if (options.emit) {
+    VuePlyr.props.emit.default = () => { return [ ...options.emit ] }
+  }
+  Vue.component(VuePlyr.name, VuePlyr)
 }
 
 // Credit to https://github.com/irazasyed for this auto Vue.use() when
 // installing from unpkg or similar.
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(VuePlyrPlugin)
+  window.Vue.use(VuePlyr)
 }
 
-export {
-  VuePlyr,
-  VuePlyrPlugin
-}
-
-export default VuePlyrPlugin
+export default VuePlyr
