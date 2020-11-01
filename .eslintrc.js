31172eea052c6c92228f5b45f9027acf58bb96cb
Gabe Dunn <gabe@gabedunn.dev>
2020-11-01 15:42:27 -0800

    update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/recommended',
-    '@vue/standard'
-  ],
-  rules: {
-    'vue/script-indent': [
-      'error', 2, {
-        'baseIndent': 1
-      }],
-    'indent': 'off',
-    'indent-legacy': ['error', 2],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'plugin:vue/vue3-recommended',
+		'eslint:recommended',
+		'@vue/prettier'
+	],
+	rules: {
+		'vue/script-indent': [
+			'error',
+			'tab',
+			{
+				baseIndent: 1
+			}
+		],
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+	},
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 }
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	useTabs: true,
+	arrowParens: 'avoid',
+	proseWrap: 'always',
+	trailingComma: 'none',
+	semi: false,
+	singleQuote: true,
+	vueIndentScriptAndStyle: true
+}
