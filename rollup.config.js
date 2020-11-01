026c4639aa824b5795f30ef10401d427b32d9560
Gabe Dunn <gabe@gabedunn.dev>
2020-10-31 18:15:44 -0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,129 @@
 import vue from 'rollup-plugin-vue'
-import resolve from 'rollup-plugin-node-resolve'
+import buble from '@rollup/plugin-buble'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+import resolve from '@rollup/plugin-node-resolve'
+import css from 'rollup-plugin-css-only'
+import minimist from 'minimist'
+
 import pkg from './package.json'
 
-// default config options
-const config = {
-  input: './src/main.js',
-  plugins: [
-    resolve({
-      only: ['vue-runtime-helpers']
-    }),
-    vue()
-  ],
-  output: {
-    file: pkg.module,
-    format: 'esm'
-  },
-  external: ['plyr']
-}
+const argv = minimist(process.argv.slice(2))
 
-export default [
-  {
-    // ESM module
-    ...config
-  },
-  {
-    // SSR module
-    ...config,
-    plugins: [
-      resolve({
-        only: ['vue-runtime-helpers']
+const baseConfig = {
+  input: 'src/index.js',
+  plugins: {
+    preVue: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
       }),
-      vue({
-        template: {
-          optimizeSSR: true
+      commonjs()
+    ],
+    vue: {
+      css: true,
+      template: {
+        isProduction: true
+      }
+    },
+    postVue: [
+      buble({
+        transforms: {
+          dangerousForOf: true
         }
       })
-    ],
+    ]
+  }
+}
+
+// Customize configs for individual targets
+const buildFormats = []
+if (!argv.format || argv.format === 'es') {
+  const esConfig = {
+    ...baseConfig,
     output: {
-      file: pkg.ssr,
-      format: 'cjs'
-    }
-  },
-  {
-    // Browser build
-    ...config,
+      file: pkg.module,
+      format: 'esm',
+      exports: 'named',
+      sourcemap: true
+    },
+    plugins: [
+      ...baseConfig.plugins.preVue,
+      css({
+        output: pkg.style
+      }),
+      vue({
+        ...baseConfig.plugins.vue,
+        css: false
+      }),
+      ...baseConfig.plugins.postVue,
+      terser({
+        output: {
+          ecma: 6
+        }
+      }),
+      resolve()
+    ]
+  }
+  buildFormats.push(esConfig)
+}
+
+if (!argv.format || argv.format === 'cjs') {
+  const umdConfig = {
+    ...baseConfig,
     output: {
+      compact: true,
+      file: pkg.main,
+      format: 'cjs',
+      name: 'VuePlyr',
+      exports: 'named',
+      sourcemap: true
+    },
+    plugins: [
+      ...baseConfig.plugins.preVue,
+      css({
+        output: pkg.style
+      }),
+      vue({
+        ...baseConfig.plugins.vue,
+        template: {
+          ...baseConfig.plugins.vue.template,
+          optimizeSSR: true
+        },
+        css: false
+      }),
+      ...baseConfig.plugins.postVue,
+      resolve()
+    ]
+  }
+  buildFormats.push(umdConfig)
+}
+
+if (!argv.format || argv.format === 'iife') {
+  const unpkgConfig = {
+    ...baseConfig,
+    output: {
+      compact: true,
       file: pkg.unpkg,
       format: 'iife',
       name: 'VuePlyr',
-      globals: {
-        plyr: 'Plyr'
-      }
-    }
+      exports: 'named',
+      sourcemap: true
+    },
+    plugins: [
+      ...baseConfig.plugins.preVue,
+      vue(baseConfig.plugins.vue),
+      ...baseConfig.plugins.postVue,
+      terser({
+        output: {
+          ecma: 5
+        }
+      }),
+      resolve()
+    ]
   }
-]
+  buildFormats.push(unpkgConfig)
+}
+
+// Export config
+export default buildFormats
