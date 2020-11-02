c3823208d6f852fb4e2fd6ce906eb0983dcab7d3
Gabe Dunn <gabe@gabedunn.dev>
2020-11-02 15:31:52 -0800

    remove testing

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	moduleFileExtensions: ['js', 'json', 'vue'],
-	testEnvironment: 'jest-environment-jsdom',
-	transform: {
-		'.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
-		'^.+\\.js$': '<rootDir>/node_modules/babel-jest'
-	}
-}
