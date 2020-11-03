11dc356a7e4db70815273c7f142e34fe4e5ee80a
Gabe Dunn <gabe@gabedunn.dev>
2020-11-02 16:24:07 -0800

    add shared example components

---
+++ examples/shared/demo.vue
@@ -0,0 +1,48 @@
+<template>
+	<div id="app">
+		<h1>vue-plyr demo</h1>
+		<div class="components">
+			<vue-plyr-video />
+			<vue-plyr-audio />
+			<youtube />
+			<youtube-np />
+			<vimeo />
+			<vimeo-np />
+		</div>
+	</div>
+</template>
+
+<script>
+	import VuePlyrVideo from './video'
+	import VuePlyrAudio from './audio'
+	import Youtube from './youtube'
+	import YoutubeNp from './youtube-np'
+	import Vimeo from './vimeo'
+	import VimeoNp from './vimeo-np'
+
+	export default {
+		name: 'Demo',
+		components: {
+			VuePlyrVideo,
+			VuePlyrAudio,
+			Youtube,
+			YoutubeNp,
+			Vimeo,
+			VimeoNp
+		}
+	}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
+
+	h1 {
+		margin: 3rem;
+	}
+</style>
---
+++ examples/shared/youtube-np.vue
@@ -0,0 +1,8 @@
+<template>
+	<div>
+		<h2>YouTube (Not Progressive-Enhancement)</h2>
+		<vue-plyr>
+			<div data-plyr-provider="youtube" data-plyr-embed-id="bTqVqk7FSmY"></div>
+		</vue-plyr>
+	</div>
+</template>
